9d90c66f07bc37a40f19477c49941a9415ceb272
densolo <programmistsolo@gmail.com>
2022-04-17 17:00:15 +0300

    пробую что такое парсинг и создаю паттерны

--- BetaPolet/example.js
+++ BetaPolet/example.js
@@ -1,4 +1,21 @@
-function func() {
-//что то тут
-    //изменение
+var digit = {
+    exec: function (str, pos) {
+        var chr = str.charAt(pos);
+            return { simbol: chr, end: pos + 1};
+    }
+};
+console.log(digit.exec("abcde", 0));
+
+function Pattern(exec) {
+    this.exec = exec;
 }
+
+function txt(text) {
+    return new Pattern(function (str, pos) {
+        if (str.substr(pos, text.length) == text)
+            return  `[ text ]
+ text: ${text}
+ end: ${pos + text.length}`;
+    });
+}
+console.log(txt("abcde").exec("abcde", 0));
