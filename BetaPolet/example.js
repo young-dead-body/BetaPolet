f26e7f63daa48f0335ecb9aad815bf2ae740653c
densolo <programmistsolo@gmail.com>
2022-04-24 16:51:31 +0300

    добавил фигни

--- BetaPolet/example.js
+++ BetaPolet/example.js
@@ -1,21 +1,64 @@
-var digit = {
-    exec: function (str, pos) {
-        var chr = str.charAt(pos);
-            return { simbol: chr, end: pos + 1};
+let n = new Map;
+var buf = new Array;
+var bufS = new String;
+var bufSR = new Array;
+var alf = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "/", ".", ",", "_"];
+var chisla = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var exp = "[скорости]  первая_скорость= 50  вторая_скорость= 123.34 [напряжения]   первое= 7  второе= 450+";
+function simbl(str) {
+    switch (str) {
+        case "[": return 1;
+        case "]": return 1;
+        case "=": return 2;
+        case ":": return 3;
+        case "+": return 4;
+        case " ": return 5;
+        default: return 0;
     }
-};
-console.log(digit.exec("abcde", 0));
-
-function Pattern(exec) {
-    this.exec = exec;
 }
-
-function txt(text) {
-    return new Pattern(function (str, pos) {
-        if (str.substr(pos, text.length) == text)
-            return  `[ text ]
- text: ${text}
- end: ${pos + text.length}`;
-    });
+var cicle = 0;
+function slovo(str, alb) {
+    let ch;
+    let stop;
+    let arr = new String;
+    for (var i = 0; i < str.length; i++) {
+        if (simbl(str[i]) != 0) {
+            bufS = bufS + simbl(str[i]);
+            exp = str.substr(i + 1, str.length);
+            break;
+        }
+        ch = str[i];
+        for (let j = 0; j < alb.length; j++) {
+            if (ch == alb[j] || ch == chisla[j])
+                arr = arr + ch;
+        }
+    }
+    cicle = simbl(str[i]);
+    return arr;
 }
-console.log(txt("abcde").exec("abcde", 0));
+var s = 0
+f = 0;
+var prov;
+
+while (cicle != 4) {
+    prov = slovo(exp, alf);
+    console.log(prov);
+    if (prov != '') {
+        buf[s] = prov;
+        s++;
+    }
+}
+for (let ci = 0; ci < bufS.length; ci++) {
+    if (bufS[ci] != 5) {
+        bufSR[ci + f] = bufS[ci];
+    } else f--;
+}
+console.log("___________");
+for (const am of buf) {
+    console.log(am);
+}
+for (const ar of bufSR) {
+    console.log(ar);
+}
+function myFunction(){
+document.getElementById("demo").innerHTML = "русский";}
