7f0cb4db163edd20480402321b80901d76e406eb
densolo <programmistsolo@gmail.com>
2022-04-28 10:29:45 +0300

    закончил черновую версию, в дальнейшем собираюсь ужать и упростить, также нужно либо добавить чтение регистра, либо понижать его перед чтением.

--- BetaPolet/expa.js
+++ BetaPolet/expa.js
@@ -1,16 +1,18 @@
-let n = new Set;
+function myFunction() {
+let n = new Map;
+var cde = new Array;
 var buf = new Array;
 var bufS = new String;
 var bufSR = new Array;
-var alf = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "/", ".", ",", "_"];
+var alf =["/", ".", ",", "_","q", "w", "e", "r","t", "y", "u", "i","o", "p", "a", "s", "d", "f", "g","h", "j", "k", "l", "z", "x", "c","v", "b", "n", "m"];
+var alfR = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "/", ".", ",", "_"];
 var chisla = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var exp = "[скорости]  первая_скорость= 50  вторая_скорость= 123.34 [напряжения]   первое= 7  второе= 450+";
-
+var exp=new String;
+    exp = document.getElementById("in").innerText;
 
 function simbl(str) {
     switch (str) {
         case "[": return 1;
-        case "]": return 1;
         case "=": return 2;
         case ":": return 3;
         case "+": return 4;
@@ -55,16 +57,32 @@ for (let ci = 0; ci < bufS.length; ci++) {
         bufSR[ci + f] = bufS[ci];
     } else f--;
 }
-   // var text= new Array();
+var text= new String;
 console.log("___________");
-for (const am of buf) {
+/*for (const am of buf) {
     console.log(am);
 }
 for (const ar of bufSR) {
     console.log(ar);
-}
-
-function myFunction() {
-    document.getElementById("out").innerText = buf;
-   // document.getElementById("in").innerText = bufSR;
-};
+}*/
+    for (const ar of bufS) {
+        console.log(ar);}
+ //for(let s=0;s<buf.length;s++){
+ //text =`{"prev": { "name": ${buf[t]}, "param": 234.1}}`;
+ //var stroka = JSON.stringify(test)
+//console.log(cde);
+   // exp = document.getElementById("in").innerText;
+    text+=`{"${buf[0]}": {`
+    for(var w=1;bufS[w]!="4";w++) {
+        switch(bufS[w]) {
+            case "1":{  text += `}, "${buf[w]}": {`; break;}
+            case "2": { if(bufS[w+1]=="1" || bufS[w+1]=="3"){ text +=`: ${buf[w]} `} else {text +=`: ${buf[w]} ,`}; break;}
+           // case "3": {text +=`}}`; break;}
+            case "5": {text +=` "${buf[w]}" `;break;}
+            default: text +=`}}`;
+        }
+    }
+    console.log(text);
+    document.getElementById("out").innerText = text;
+    };
+myFunction();
